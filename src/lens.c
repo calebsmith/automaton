20511e65c557f85a7b46fe6cee806dc597771f9d
Caleb Smith <caleb.smithnc@gmail.com>
2013-10-13 15:58:46 -0400

    First pass at making lens able to zoom in/out

--- src/lens.c
+++ src/lens.c
@@ -188,3 +188,17 @@ void lens_move_down(Lens_t* lens)
 {
     lens_move(lens, DOWN);
 }
+
+void lens_zoom_in(Lens_t* lens)
+{
+    if (lens->scale < 20) {
+        lens->scale++;
+    }
+}
+
+void lens_zoom_out(Lens_t* lens)
+{
+    if (lens->scale > 1) {
+        lens->scale--;
+    }
+}
