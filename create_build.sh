75876d3c76981d14022f2c65e1e478ac75712918
Caleb Smith <caleb.smithnc@gmail.com>
2014-06-14 11:54:55 -0400

    Adds a shell script for creating a "build" (tarball with everything)

---
+++ create_build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+BUILDDIR="game_of_life"
+mkdir $BUILDDIR
+echo "Compiling ..."
+make
+echo "Copying docs into" $BUILDDIR
+cp LICENSE $BUILDDIR
+cp README.md $BUILDDIR
+cp -r docs $BUILDDIR
+echo "Copying data assets and Scheme code into" $BUILDDIR
+cp -r rules $BUILDDIR
+cp -r data $BUILDDIR
+cp -r scm $BUILDDIR
+echo "Copying executables into" $BUILDDIR
+cp gameoflife $BUILDDIR
+echo "Copying shell scripts into" $BUILDDIR
+cp run_*.sh $BUILDDIR
+echo "Compressiong" $BUILDDIR
+tar -cvf gameoflife.tar $BUILDDIR
+echo "Cleaning up"
+rm -Rf $BUILDDIR
