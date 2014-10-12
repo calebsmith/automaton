9f2c1f77a36ae631027f82de843f47e32afd7c3b
Caleb Smith <caleb.smithnc@gmail.com>
2014-10-12 17:17:23 -0400

    Add memoization to scm/gameoflife.scm to improve performance

--- scm/gameoflife.scm
+++ scm/gameoflife.scm
@@ -1,6 +1,8 @@
 (define-module (gameoflife)
     #:export (generate-next))
 
+(use-modules (ice-9 poe))
+
 
 (define (generate-cell current neighbors)
     (if (= current 0)
@@ -10,6 +12,11 @@
         (if (or (= neighbors 2) (= neighbors 3))
             1
             0)))
+
+; Memoize pure functions and primitive procs that return a constant value
+(define M_generate-cell (pure-funcq generate-cell))
+(define M_width-range (pure-funcq (lambda () (iota (board-get-width)))))
+(define M_height-range (pure-funcq (lambda () (iota (board-get-height)))))
 
 
 (define (cell-based-generator x y state-num neighbor-func rule-func)
@@ -21,6 +28,6 @@
 (define (generate-next)
     (map (lambda (y)
         (map (lambda (x)
-            (cell-based-generator x y 1 board-moore-neighbors generate-cell))
-            (iota (board-get-width))))
-        (iota (board-get-height))))
+            (cell-based-generator x y 1 board-moore-neighbors M_generate-cell))
+            (M_width-range)))
+        (M_height-range)))
